7d4e67dea1439783fae84611fac4dd5cf17498bc
Tasso1982 <tasso19820814@yahoo.co.jp>
2021-01-08 14:26:12 +0000

    Task for Rails 04 -Solve potential issues on user registration-

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 15 }
+  
+  # https://qiita.com/jnchito/items/893c887fbf19e17d3ff9
+  # http://emailregex.com/ は上手くいかなかった・・・
+  # 結局、シンプルに　https://qiita.com/shumpeism/items/d299aaeb34e1d741e82b
+  Valid_Email_Regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: Valid_Email_Regex }
+  
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 8, maximum: 32 }, format: { with: VALID_PASSWORD_REGEX }
+  
+end
